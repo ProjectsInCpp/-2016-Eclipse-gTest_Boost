ef35ac0a53c24cb6e35ed57a1527bad754007872
trunksBT <korytko07@gmail.com>
2016-07-03 12:28:45 +0200

    T1: Created Sample Project with gTest and Boost_Foreach

---
+++ CShape/src/CShape.cpp
@@ -0,0 +1,24 @@
+/*
+ * CShape.cpp
+ *
+ *  Created on: Jul 3, 2016
+ *      Author: pkorycin
+ */
+
+#include "CShape.h"
+
+CShape::CShape() {
+	// TODO Auto-generated constructor stub
+
+}
+
+
+CShape::~CShape() {
+	// TODO Auto-generated destructor stub
+}
+
+int CShape::GetID()
+{
+	return 1;
+}
+
---
+++ CShape/src/CShape.h
@@ -0,0 +1,18 @@
+/*
+ * CShape.h
+ *
+ *  Created on: Jul 3, 2016
+ *      Author: pkorycin
+ */
+
+#ifndef SRC_CSHAPE_H_
+#define SRC_CSHAPE_H_
+
+class CShape {
+public:
+	int GetID();
+	CShape();
+	virtual ~CShape();
+};
+
+#endif /* SRC_CSHAPE_H_ */
